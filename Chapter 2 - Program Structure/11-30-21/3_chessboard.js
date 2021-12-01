eedb4fcd6a0cdf213b6d45624fae02fdfe452cbd
Edward Blanciak <blanciak.edward2@gmail.com>
2021-11-30 20:05:37 -0500

    attempted and book solution

--- Chapter 2 - Program Structure/11-30-21/3_chessboard.js
+++ Chapter 2 - Program Structure/11-30-21/3_chessboard.js
@@ -11,3 +11,44 @@
 // # # # #
 //  # # # #
 // # # # #
+
+// take input for a grid size
+// create a standard n x n grid
+
+// Incorrect
+const createGrid = (size) => {
+	let grid = '';
+
+	for (let y = 0; y < size; y++) {
+		if (y >= 1) {
+			grid += '\n';
+		}
+
+		for (let x = 0; x < size; x++) {
+			if (x % 2 == 0) {
+				grid += '#';
+			} else {
+				grid += ' ';
+			}
+		}
+	}
+
+	console.log(grid);
+};
+
+createGrid(8);
+
+// Book solution
+let size = 8;
+let board = '';
+
+for (let y = 0; y < size; y++) {
+	for (let x = 0; x < size; x++) {
+		if ((x + y) % 2 == 0) {
+			board += ' ';
+		} else {
+			board += '#';
+		}
+	}
+	board += '\n';
+}
